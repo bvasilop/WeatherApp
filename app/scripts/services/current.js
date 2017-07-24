4190b956f57cb2c11c2c204fc5a4975d38268d03
bvasilop <bvasilop@gmail.com>
2017-07-23 20:35:26 -0700

    added data for creating weather app

---
+++ app/scripts/services/current.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name bootstrappingAngularJsAppApp.current
+ * @description
+ * # current
+ * Factory in the bootstrappingAngularJsAppApp.
+ */
+angular.module('bootstrappingAngularJsAppApp')
+  .factory('current', function ($resource) {
+    // Service logic
+    // Public API Here:
+        return $resource('http://api.openweathermap.org/data/2.5/weather?q=:location&units=imperial&APPID=a1229545e71f4cf85672eee69b3b302e', {}, {
+          query: {
+              method:'GET',
+              params:{
+                location: 'Seattle,us'
+              },
+              isArray:false
+            }
+      });
+    });
