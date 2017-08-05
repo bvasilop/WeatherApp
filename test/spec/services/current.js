f28b40f162c6eb0c015cc632f6e97fad6091819a
bvasilop <bvasilop@gmail.com>
2017-08-04 17:02:33 -0700

    added local storage to save cities

--- test/spec/services/current.js
+++ test/spec/services/current.js
@@ -14,5 +14,16 @@ describe('Service: current', function () {
   it('should do something', function () {
     expect(!!current).toBe(true);
   });
+  angular.module('yourApp')
+    .controller('MainCtrl', function ($scope, citysearch, $localStorage) {
+      $scope.citiesFound = citysearch.find();
+      $scope.storage = $localStorage;
 
+      $scope.findCities = function(){
+          $scope.citiesFound = citysearch.find({
+              query: $scope.location
+          });
+          $scope.searchQuery = $scope.location;
+      };
+    });
 });
