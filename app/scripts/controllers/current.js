4906f0565a395ac65e5dc64a4779e7c01bd47f50
bvasilop <bvasilop@gmail.com>
2017-08-04 22:21:46 -0700

    removed = from line30 line35 scripts/controllers/current.js

--- app/scripts/controllers/current.js
+++ app/scripts/controllers/current.js
@@ -27,12 +27,12 @@ angular.module('bootstrappingAngularJsAppApp')
         var save = true; // Initialize the save decision variable.
         // Use this loop to check if we've already saved the city.
         for (var i=0; i < $localStorage.savedCities.length; i++){
-            if ($localStorage.savedCities[i].id === cityData.id) {
+            if ($localStorage.savedCities[i].id == cityData.id) {
                 // This is a duplicate, so don't save (variable set to false).
                 save = false;
             }
         }
-        if (save===true){
+        if (save==true){
             $localStorage.savedCities.push(cityData);
         } else {
             console.log('city already saved');
