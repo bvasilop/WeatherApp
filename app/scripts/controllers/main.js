cf2498f1b09d92c03706dcf7dccc228c55ef0710
bvasilop <bvasilop@gmail.com>
2017-08-04 17:27:27 -0700

    added saved cities on home screen

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,9 +8,9 @@
  * Controller of the bootstrappingAngularJsAppApp
  */
 angular.module('bootstrappingAngularJsAppApp')
-  .controller('MainCtrl', function ($scope, citysearch) {
+  .controller('MainCtrl', function ($scope, citysearch, $localStorage) {
     $scope.citiesFound = citysearch.find();
-
+    $scope.storage = $localStorage;
     $scope.findCities = function(){
            $scope.citiesFound = citysearch.find({
                query: $scope.location
